6d8a30f0fee057a0647b0adaba54f9281bb865d4
Daanielaih <daniela.perez4@mail.udp.cl>
2023-12-11 00:14:47 -0300

    fbf
    fgfb

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/danielaperez/Documents/GitHub/Portafolio/.cache/dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/Users/danielaperez/Documents/GitHub/Portafolio/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("/Users/danielaperez/Documents/GitHub/Portafolio/src/pages/404.tsx"))
+}
+
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,11 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Portafolio`,
+    siteTitleAlt: `Portafolio`,
+    siteHeadline: `Portafolio`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Vida de danielai`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
